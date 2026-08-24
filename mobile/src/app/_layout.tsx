18bdfa655af416c5ae743a7124d6e355ad4573b6
Marcelo Pereira Barbosa <marceloppb@outlook.com>
2026-08-24 19:50:57 -0300

    chore: ignore all logs in root layout to suppress development warnings

--- mobile/src/app/_layout.tsx
+++ mobile/src/app/_layout.tsx
@@ -1,6 +1,9 @@
 import { Stack } from "expo-router";
 
 import { AppProvider } from "@/infrastructure/providers/app-provider";
+import { LogBox } from "react-native";
+
+LogBox.ignoreAllLogs(true)
 
 export default function RootLayout() {
   return (
